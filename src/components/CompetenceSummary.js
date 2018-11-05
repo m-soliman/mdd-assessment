17f4ab7cdcaec56a885cd53cc5c34cf56e12daf7
Maria Soliman <maria@eineband.de>
2018-11-05 20:27:03 +0100

    Display overview as list

--- src/components/CompetenceSummary.js
+++ src/components/CompetenceSummary.js
@@ -8,16 +8,20 @@ class CompetenceSummary extends React.Component {
                 <h3>Summary of competence {this.props.competenceDescription}</h3>
                 <i>Todo: This should be graphical :)</i>
 
-                {this.props.indicators.map((indicator, i) =>
-                    (<div>
-                        <h4>{indicator.description}</h4>
-                        <a onClick={this.props.showIndicator.bind(this, i)}>
-                            {this.props.levelSelections !== null && this.props.levelSelections.hasOwnProperty(indicator.id)
-                                ? indicator.levels[this.props.levelSelections[indicator.id]].description
-                                : 'give answer'
-                            }</a>
-                    </div>)
-                )}
+                <ul className="list">
+                    {this.props.indicators.map((indicator, i) =>
+                        (
+                            <li>
+                                <h4>{indicator.description}</h4>
+                                <a onClick={this.props.showIndicator.bind(this, i)}>
+                                    {this.props.levelSelections !== null && this.props.levelSelections.hasOwnProperty(indicator.id)
+                                        ? indicator.levels[this.props.levelSelections[indicator.id]].description
+                                        : 'give answer'
+                                    }</a>
+                            </li>
+                        )
+                    )}
+                </ul>
 
             </div>
         )
